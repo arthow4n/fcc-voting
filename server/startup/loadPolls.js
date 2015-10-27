ac78b4575a9f1dd8908c4015642dadcf7d5a78c1
arthow4n <hevark@msn.com>
2015-10-27 12:39:09 +0000

    collection allow logic added

--- server/startup/loadPolls.js
+++ server/startup/loadPolls.js
@@ -1,8 +1,18 @@
 Meteor.startup( function () {
     if (Polls.find().count() === 0) {
         var polls = [
-                { id: "ciMc3oXug4Yyu3gvp"}
-            
+                { owner: "ciMc3oXug4Yyu3gvp",
+                    title: "What OS do you use?",
+                    results: { "Windows": 10,
+                                "Linux": 25,
+                                "OSX": 8}
+                },
+                { owner: "ciMc3oXug4Yyu3gvp",
+                    title: "What DE do you use?",
+                    results: { "KDE": 20,
+                                "Xfce": 20,
+                                "Cinnamon": 8}
+                }
             ];
         for (var i = 0; i < polls.length; i++) {
             Polls.insert(polls[i]);
