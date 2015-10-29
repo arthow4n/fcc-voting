b17547a430856f6e5282b2f8709af17ff2b9da9c
arthow4n <hevark@msn.com>
2015-10-29 13:38:01 +0000

    prevent unauthed user access of newPoll and myPolls

--- client/routes.ng.js
+++ client/routes.ng.js
@@ -1,55 +1,58 @@
-angular.module("fcc-voting").run( function ($rootScope, $state) {
-    $rootScope.$on("stateChangeError", function (event, toState, toParams, fromState, fromParams, error) {
-       if (error === "AUTH_REQUIRED") {
-           $state.go("pollsList");
-       } 
-    });
-});
+angular.module("fcc-voting")
+
+    .run(["$rootScope", "$state", function($rootScope, $state) {
+      $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
+        if (error === "AUTH_REQUIRED") {
+            $state.go("pollsList");
+        }
+      });
+    }])
+    
 
 
-angular.module("fcc-voting").config(
-    function ($urlRouterProvider, $stateProvider, $locationProvider) {
-    
-    $locationProvider.html5Mode(true);
-    
-    $stateProvider
-        .state("pollsList", {
-            url: "/polls",
-            templateUrl: "client/polls/views/polls-list.ng.html",
-            controller: "PollsListCtrl",
-            data: {
-                showMyPollsOnly: false
-            }
-        })
-        .state("myPolls", {
-            url: "/mypolls",
-            templateUrl: "client/polls/views/polls-list.ng.html",
-            controller: "PollsListCtrl",
-            data: {
-                showMyPollsOnly: true
-            },
-            resolve: {
-                currentUser: function ($meteor) {
-                    return $meteor.requireUser();
-                }
-            }
-        })
-        .state("newPoll", {
-            url: "/newpoll",
-            templateUrl: "client/polls/views/new-poll.ng.html",
-            controller: "NewPollCtrl",
-            resolve: {
-                currentUser: function ($meteor) {
-                    return $meteor.requireUser();
-                }
-            }
-        })
-        .state("pollDetails", {
-            url: "/polls/:pollId",
-            templateUrl: "client/polls/views/poll-details.ng.html",
-            controller: "PollDetailsCtrl"
-        });
+    .config(
+        function ($urlRouterProvider, $stateProvider, $locationProvider) {
         
-    $urlRouterProvider.otherwise("/polls");
+        $locationProvider.html5Mode(true);
+        
+        $stateProvider
+            .state("pollsList", {
+                url: "/polls",
+                templateUrl: "client/polls/views/polls-list.ng.html",
+                controller: "PollsListCtrl",
+                data: {
+                    showMyPollsOnly: false
+                }
+            })
+            .state("myPolls", {
+                url: "/mypolls",
+                templateUrl: "client/polls/views/polls-list.ng.html",
+                controller: "PollsListCtrl",
+                data: {
+                    showMyPollsOnly: true
+                },
+                resolve: {
+                    currentUser: function ($meteor) {
+                        return $meteor.requireUser();
+                    }
+                },
+            })
+            .state("newPoll", {
+                url: "/newpoll",
+                templateUrl: "client/polls/views/new-poll.ng.html",
+                controller: "NewPollCtrl",
+                resolve: {
+                    currentUser: function ($meteor) {
+                        return $meteor.requireUser();
+                    }
+                }
+            })
+            .state("pollDetails", {
+                url: "/polls/:pollId",
+                templateUrl: "client/polls/views/poll-details.ng.html",
+                controller: "PollDetailsCtrl"
+            });
+            
+        $urlRouterProvider.otherwise("/polls");
     
 });
